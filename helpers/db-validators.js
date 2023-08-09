be86af6f4bc3af5d1fa44c23ef1901df6ce25142
Victor Manuel Gonzalez Del Vasto <vgonzalez@jpglobaldigital.com>
2023-08-09 11:56:48 -0700

    CRUD created and validated

---
+++ helpers/db-validators.js
@@ -0,0 +1,27 @@
+const Role = require('../models/role')
+const User = require('../models/users')
+const isRoleValid = async (rol = '') => {
+    const rolExist = await Role.findOne({rol})
+    if(!rolExist){
+        throw new Error(`${rol} rol is not valid`)
+    }
+}
+
+const isEmailExist = async (email = '') => {
+    const emailExist = await User.findOne({email})
+    if (emailExist) {
+        throw new Error('The email exist')
+    }
+}
+const existUserById = async (id = '') => {
+    const existUserById = await User.findOne({id})
+    if (!existUserById) {
+        throw new Error(`The id: ${id} doesn't exist`)
+    }
+}
+
+module.exports = {
+    isRoleValid,
+    isEmailExist,
+    existUserById
+}
--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,31 @@
 const { Router } = require('express')
-
+const { check } = require('express-validator')
 const {getUsers, updateUser, addUser, deleteUser} = require('../controllers/users')
+const { isRoleValid, isEmailExist, existUserById } = require('../helpers/db-validators')
+const { validateFields } = require('../middlewares/validate-fields')
+
 
 const router = Router()
 
 router.get('/', getUsers)
-router.put('/:id', updateUser)
-router.post('/', addUser)
-router.delete('/', deleteUser)
+router.put('/:id',[
+    check('id', 'Is not valid ID').isMongoId(),
+    check('id').custom(existUserById),
+    check('role').custom(isRoleValid),
+    validateFields
+],updateUser)
+router.post('/',[ 
+    check('name','name field field is required').not().isEmpty(),
+    check('password','password field is required').isLength({ min: 6}),
+    check('email','email is not valid').custom(isEmailExist),
+    check('role').custom(isRoleValid),
+    // check('role','Is not a valid role').isIn(['admin','user']),
+    validateFields
+], addUser)
+router.delete('/:id',[
+    check('id', 'Is not valid ID').isMongoId(),
+    // check('id').custom(existUserById),
+    validateFields
+], deleteUser)
 
 module.exports = router
